178d1cc72f001e1db19f300f01246e8564c46150
Toma Otani <otani.toma@gmail.com>
2020-05-06 22:08:28 -0700

    Updates

---
+++ precache-manifest.1016c8ec9ba6f61de56b46e2503f5120.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51721fa868d11d09e2417c35f1202090",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "aa4aa88c471f479452c6",
+    "url": "/covid-19/static/css/main.ed58d7fe.chunk.css"
+  },
+  {
+    "revision": "e08f8ad8d84e140ad3a6",
+    "url": "/covid-19/static/js/2.ba1eddeb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.ba1eddeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4aa88c471f479452c6",
+    "url": "/covid-19/static/js/main.f4151db6.chunk.js"
+  },
+  {
+    "revision": "c59e2f922549c307064d",
+    "url": "/covid-19/static/js/runtime-main.aabceb11.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
