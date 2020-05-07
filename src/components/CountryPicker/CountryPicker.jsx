aa7ede432f0ded7b94dbd4beaed7285bb3abe9b1
Toma Otani <otani.toma@gmail.com>
2020-05-06 21:59:40 -0700

    fix bug: error when back to global

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -24,7 +24,7 @@ const CountryPicker = ({ handleCountryChange }) => {
         defaultValue=""
         onChange={(e) => handleCountryChange(e.target.value)}
       >
-        <option value="global">Global</option>
+        <option value="">Global</option>
         {fetchedCountries.map((country, index) => <option key={index} value={country}>{country}</option> )}
       </NativeSelect>
     </FormControl>
